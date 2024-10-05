8e9b688b7f88855ef659545061f5bf56032553c1
RahmaFahrurrozi <zfahrur150105@gmail.com>
2024-10-05 19:43:53 +0700

    finish structure

---
+++ src/ContactApp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ContactList from "./ContactList";
+import { getData } from "./data";
+
+const ContactApp = () => {
+  const contacts = getData();
+  return (
+    <div className="contact-app">
+      <h1>Aplikasi Kontak</h1>
+      <h2>Daftar Kontak</h2>
+      <ContactList contacts={contacts} />
+    </div>
+  );
+};
+
+export default ContactApp;
